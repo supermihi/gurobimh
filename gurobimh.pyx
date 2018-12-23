6e0229bd3d10f46ccf26319789b64d9aac8a1271
Michael Helmling <michaelhelmling@posteo.de>
2018-12-23 13:59:49 +0100

    Update to gurobi81

--- gurobimh.pyx
+++ gurobimh.pyx
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# distutils: libraries = ["gurobi65"]
+# distutils: libraries = ["gurobi81"]
 # cython: boundscheck=False
 # cython: nonecheck=False
 # cython: wraparound=False
